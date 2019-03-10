69b646d450e5aa2cce0b66cb76e3c5a2280cce13
youssef <youed18@gmail.com>
2019-03-10 15:42:57 +0100

    creation factory et routes et controller menu

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Menu;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(Menu::class, 10)->create();
     }
 }
